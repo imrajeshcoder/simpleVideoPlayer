fa138c2e8a28e8e096dd0bba90f3aa323ccff1ea
Vishal Sharma <jayavaland92@gmail.com>
2021-01-28 17:54:29 +0530

    Initial Commit
    Co-Authored-By: Rajesh Shiyal <76999489+imrajeshcoder@users.noreply.github.com>

---
+++ simpleVideoPlayer/simpleVideoPlayer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  simpleVideoPlayer
+//
+//  Created by Vijay on 28/01/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
