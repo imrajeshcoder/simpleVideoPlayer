b52f3a52b8851914f78e66a42b3e2eca896e46e9
Vishal Sharma <jayavaland92@gmail.com>
2021-01-30 12:08:30 +0530

    final commit

--- simpleVideoPlayer/simpleVideoPlayer/ViewController.swift
+++ simpleVideoPlayer/simpleVideoPlayer/ViewController.swift
@@ -6,14 +6,36 @@
 //
 
 import UIKit
+import AVKit
+import AVFoundation
+
 
 class ViewController: UIViewController {
 
+    override func viewDidAppear(_ animated: Bool) {
+        playVideo()
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
     }
-
+       
+    private func playVideo() {
+                           
+        
+        guard let path = Bundle.main.path(forResource: "myvideo", ofType:"mp4" ) else {
+                debugPrint("myvideo.mp4 not found")
+                return
+            }
+        let player = AVPlayer(url: URL(fileURLWithPath: path))
+            let playerController = AVPlayerViewController()
+            playerController.player = player
+        
+            present(playerController, animated: true) {
+                player.play()
+            }
+        }
 
 }
 
